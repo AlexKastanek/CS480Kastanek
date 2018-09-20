614a2a0d06ca4e8462ee4b2127df7833113f1cd4
AlexKastanek <akastanek@nevada.unr.edu>
2018-09-19 23:53:58 -0700

    Finished PA4 (may need to be revised). Succesfully created a board with four walls in Blender, then exported it as an obj along with an mtl. Loaded the data from the obj using a new ObjectLoader function and successfully rendered the board.

---
+++ PA4/include/graphics.h
@@ -0,0 +1,56 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <iostream>
+using namespace std;
+
+#include "graphics_headers.h"
+#include "camera.h"
+#include "shader.h"
+#include "planet.h"
+#include "moon.h"
+
+class Graphics
+{
+  public:
+    Graphics();
+    ~Graphics();
+    bool Initialize(int width, int height);
+    void Update(unsigned int dt);
+    void Render();
+
+    bool IsPlanetPaused();
+    unsigned int GetPlanetSpin();
+    unsigned int GetPlanetOrbit();
+    bool IsMoonPaused();
+    unsigned int GetMoonSpin();
+    unsigned int GetMoonOrbit();
+
+    void SetPlanetPaused(bool paused);
+    void SetPlanetSpin(unsigned int spin);
+    void SetPlanetOrbit(unsigned int orbit);
+    void SetMoonPaused(bool paused);
+    void SetMoonSpin(unsigned int spin);
+    void SetMoonOrbit(unsigned int orbit);
+
+  private:
+    std::string ErrorString(GLenum error);
+
+    Camera *m_camera;
+    Shader *m_shader;
+
+    //struct Vertex
+    //{
+    //    GLfloat
+    //}
+
+    GLint m_projectionMatrix;
+    GLint m_viewMatrix;
+    GLint m_modelMatrix;
+
+    Object *m_board;
+    Planet *m_planet;
+    Moon *m_moon;
+};
+
+#endif /* GRAPHICS_H */
---
+++ PA4/src/object.cpp
@@ -0,0 +1,328 @@
+#include "object.h"
+
+Object::Object()
+{ 
+
+  //m_parent = new glm::mat4(1.0);
+  m_parent = NULL;
+
+  /*
+    # Blender File for a Cube
+    o Cube
+    v 1.000000 -1.000000 -1.000000
+    v 1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 -1.000000
+    v 1.000000 1.000000 -0.999999
+    v 0.999999 1.000000 1.000001
+    v -1.000000 1.000000 1.000000
+    v -1.000000 1.000000 -1.000000
+    s off
+    f 2 3 4
+    f 8 7 6
+    f 1 5 6
+    f 2 6 7
+    f 7 8 4
+    f 1 4 8
+    f 1 2 4
+    f 5 8 6
+    f 2 1 6
+    f 3 2 7
+    f 3 7 4
+    f 5 1 8
+  */
+
+  Vertices = {
+    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
+    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
+    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+  };
+
+  Indices = {
+    2, 3, 4,
+    8, 7, 6,
+    1, 5, 6,
+    2, 6, 7,
+    7, 8, 4,
+    1, 4, 8,
+    1, 2, 4,
+    5, 8, 6,
+    2, 1, 6,
+    3, 2, 7,
+    3, 7, 4,
+    5, 1, 8
+  };
+
+  // The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+
+  angleTranslate = 0.0f;
+  angleRotate = 0.0f;
+
+  m_paused = 0; //not paused
+  m_spinDirection = 0; //spinning counter-clockwise
+  m_orbitDirection = 0; //orbiting counter-clockwise
+}
+
+Object::Object(string filename)
+{
+  LoadObject(filename, &Vertices, &Indices);
+
+  cout << "Vertices:" << endl;
+  cout << "{" << endl;
+  for (int i = 0; i < Vertices.size(); i++)
+  {
+    cout << "{{"
+         << Vertices[i].vertex.x
+         << ", "
+         << Vertices[i].vertex.y
+         << ", "
+         << Vertices[i].vertex.z
+         << "}{"
+         << Vertices[i].color.x
+         << ", "
+         << Vertices[i].color.y
+         << ", "
+         << Vertices[i].color.z
+         << "}}" << endl;
+  }
+  cout << "}" << endl;
+
+  cout << "Indices:" << endl;
+  cout << "{" << endl;
+  for (int i = 0; i < Indices.size(); i++)
+  {
+    cout << Indices[i] << " ";
+    if (((i + 1) % 3) == 0) cout << endl;
+  }
+  cout << "}" << endl;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+}
+
+Object::~Object()
+{
+  m_children.clear();
+
+  Vertices.clear();
+  Indices.clear();
+}
+
+/*virtual*/ void Object::Update(unsigned int dt)
+{
+
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+bool Object::LoadObject(string in_filename, vector<Vertex>* out_vertices, vector<unsigned int>* out_indices)
+{
+  ifstream fin;
+  string fileData;
+  glm::vec3 default_color(0.5, 0.5, 0.5);
+
+  fin.open(in_filename);
+  if (!fin.is_open())
+  {
+    cout << "Could not open object file" << endl;
+    return false;
+  }
+
+  out_vertices->clear();
+  out_indices->clear();
+
+  while (fin >> fileData)
+  {
+    if (fileData == "v")
+    {
+      //cout << "vertex found" << endl;
+
+      Vertex newVertexObject(glm::vec3(1.0),glm::vec3(1.0));
+      glm::vec3 newVertex;
+      glm::vec3 newColor;
+
+      for (int i = 0; i < 3; i++)
+      {
+        float value;
+
+        fin >> fileData;
+        value = stof(fileData);
+
+        switch (i)
+        {
+          case 0:
+            newVertex.x = value;
+            break;
+          case 1:
+            newVertex.y = value;
+            break;
+          case 2:
+            newVertex.z = value;
+            break;
+          default:
+            cout << "Index out of range" << endl;
+        }
+      }
+
+      //newColor = default_color;
+      newColor.x = default_color.x;
+      newColor.y = default_color.y;
+      newColor.z = default_color.z;
+
+      //newVertexObject.vertex = newVertex;
+      newVertexObject.vertex.x = newVertex.x;
+      newVertexObject.vertex.y = newVertex.y;
+      newVertexObject.vertex.z = newVertex.z;
+      //newVertexObject.color = newColor;
+      newVertexObject.color.x = newColor.x;
+      newVertexObject.color.y = newColor.y;
+      newVertexObject.color.z = newColor.z;
+      out_vertices->push_back(newVertexObject);
+    }
+    else if (fileData == "f")
+    {
+      //cout << "index found" << endl;
+
+      for (int i = 0; i < 3; i++)
+      {
+        unsigned int newIndex;
+        int fileDataIterator = 0;
+        string vertexIndex;
+
+        fin >> fileData;
+        while (fileData[fileDataIterator] != '/')
+        {
+          vertexIndex.push_back(fileData[fileDataIterator]);
+          fileDataIterator++;
+        }
+
+        newIndex = (unsigned int) stoi(vertexIndex);
+        out_indices->push_back(newIndex - 1);
+      }
+    }
+  }
+
+  fin.close();
+
+  cout << "Load function complete" << endl;
+  return true;
+}
+
+glm::mat4 Object::GetModel()
+{
+  return model;
+}
+
+/*
+glm::mat4* Object::GetModelReference()
+{
+  return &model;
+}
+*/
+
+glm::mat4 Object::GetTranslation()
+{
+  return translation;
+}
+
+glm::mat4 Object::GetRotation()
+{
+  return rotation;
+}
+
+glm::mat4 Object::GetScale()
+{
+  return scale;
+}
+
+bool Object::IsPaused()
+{
+  return m_paused;
+}
+
+unsigned int Object::GetSpinDirection()
+{
+  return m_spinDirection;
+}
+
+unsigned int Object::GetOrbitDirection()
+{
+  return m_orbitDirection;
+}
+
+/*
+void SetParent(glm::mat4* parent)
+{
+  m_parent = parent;
+}
+*/
+
+void Object::SetParent(Object* parent)
+{
+  m_parent = parent;
+}
+
+void Object::AddChild(Object* child)
+{
+  m_children.push_back(child);
+}
+
+void Object::RemoveChild()
+{
+  if (!m_children.empty())
+  {
+    m_children.pop_back();
+  }
+}
+
+void Object::SetPaused(bool paused)
+{
+  m_paused = paused;
+}
+
+void Object::SetSpinDirection(unsigned int spinDirection)
+{
+  m_spinDirection = spinDirection;
+}
+
+void Object::SetOrbitDirection(unsigned int orbitDirection)
+{
+  m_orbitDirection = orbitDirection;
+}
