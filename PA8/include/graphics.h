b94c79048bf20dca4bb77f5c9feb60bc826dc10a
orion996 <orion996@gmail.com>
2018-10-26 20:59:52 -0700

    Added Invisible Collision Walls

---
+++ PA8/include/graphics.h
@@ -0,0 +1,43 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "graphics_headers.h"
+//#include "physics.h"
+#include "world.h"
+#include "camera.h"
+#include "shader.h"
+//#include "object.h"
+//#include "board.h"
+
+class Graphics
+{
+  public:
+    Graphics();
+    ~Graphics();
+
+    bool Initialize(int width, int height);
+    void Update(unsigned int dt);
+    void Render();
+
+  private:
+    std::string ErrorString(GLenum error);
+
+    //Physics *m_physics;
+    World *m_world;
+
+    Camera *m_camera;
+
+    Shader *m_shader;
+
+    GLint m_projectionMatrix;
+    GLint m_viewMatrix;
+    GLint m_modelMatrix;    
+};
+
+#endif /* GRAPHICS_H */
