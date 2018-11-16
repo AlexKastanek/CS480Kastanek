d8f825603fe4894ee4c9890da7b1322f281a3188
Alex Kastanek <akastanek@nevada.unr.edu>
2018-11-16 05:35:59 -0800

    Slight adjustments to plunger and flipper speed

--- PA10/src/flipper_right.cpp
+++ PA10/src/flipper_right.cpp
@@ -48,7 +48,7 @@ FlipperRight::FlipperRight(string filename, float scale, glm::vec3 position, btT
   m_thresholdAngle = -1.047197551;
   m_currentAngle = m_initialAngle;
 
-  m_flipAngleStep = 0.015f;
+  m_flipAngleStep = 0.012f;
   m_resetAngleStep = -0.004f;
 }
 
--- PA10/src/main.cpp
+++ PA10/src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
 
   // Start an engine
-  Engine *engine = new Engine("Jordy Kastanek and Trombley PA10", 960, 540);
+  Engine *engine = new Engine("Jordy Kastanek and Trombley PA10", 1600, 900);
 
   if(!engine->Initialize())
   {
